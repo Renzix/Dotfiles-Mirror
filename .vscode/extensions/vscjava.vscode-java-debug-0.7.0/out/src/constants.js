5f53e5654e5e636de51355dde960e8bf4c5054ee
Renzix <eragon9981@gmail.com>
2018-04-11 15:10:16 -0400

    modified:   .config/Code/Backups/workspaces.json 	modified:   .config/Code/Cache/data_0 	modified:   .config/Code/Cache/data_1 	modified:   .config/Code/Cache/data_3 	modified:   .config/Code/Cache/index 	modified:   .config/Code/Cookies 	modified:   .config/Code/GPUCache/data_0 	modified:   .config/Code/GPUCache/data_1 	modified:   .config/Code/GPUCache/data_3 	modified:   .config/Code/GPUCache/index 	modified:   .config/Code/Local Storage/file__0.localstorage 	modified:   .config/Code/storage.json 	modified:   .config/bspwm/bspwmrc 	deleted:    .config/bspwm/setwallpaper.sh 	deleted:    .vscode/extensions/redhat.java-0.21.0/.editorconfig

--- .vscode/extensions/vscjava.vscode-java-debug-0.7.0/out/src/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JAVA_LANGID = "java";
-exports.HCR_EVENT = "hotcodereplace";
-exports.USER_NOTIFICATION_EVENT = "usernotification";
-//# sourceMappingURL=constants.js.map
