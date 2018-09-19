9f5e14f13f0b13d45ea18c6fcbbc19432771ea84
Daniel <eragon9981@gmail.com>
2018-09-19 17:41:50 -0400

    changed .emacs

---
+++ .emacs.d/elpa/git-commit-20180912.1012/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20180912.1012" "Edit Git commit messages" '((emacs "25.1") (dash "20180413") (with-editor "20180414")) :commit "8adbe43554d5a36acf1de6a23f4395bfa82a85e2" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
