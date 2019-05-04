229b7396067ee10d2e083e2f87d9c2210af4d4a1
Daniel DeBruno <eragon9981@gmail.com>
2019-05-04 13:47:16 -0400

    Evil snipe stuff

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -11,8 +11,8 @@
   "Renames current buffer and file it is visiting."
   (interactive)
   (let* ((name (buffer-name))
-        (filename (buffer-file-name))
-        (basename (file-name-nondirectory filename)))
+         (filename (buffer-file-name))
+         (basename (file-name-nondirectory filename)))
     (if (not (and filename (file-exists-p filename)))
         (error "Buffer '%s' is not visiting a file!" name)
       (let ((new-name (read-file-name "New name: " (file-name-directory filename) basename nil basename)))
@@ -28,6 +28,7 @@
 ;; Variables/Hooks
 (add-hook 'after-save-hook '(lambda () (async-start (bookmark-set (buffer-name) nil))))
 (add-hook 'after-save-hook '(lambda () (async-start (bookmark-set "LastSave" nil))))
+(add-hook 'magit-mode-hook 'turn-off-evil-snipe-override-mode)
 (setq display-line-numbers-type 'relative
       display-line-numbers-current-absolute t
       display-line-numbers-width 4
@@ -38,7 +39,8 @@
                                kept-new-versions 10
                                kept-old-versions 10
                                version-control t)
-      vterm-shell "ion")
+      vterm-shell "ion"
+      evil-snipe-spillover-scope 'buffer)
 
 
 
@@ -46,32 +48,35 @@
 (map!
  :n ";" #'counsel-M-x
  :n "," (general-simulate-key "SPC m")
+ :n ">" #'evil-snipe-repeat
+ :n "<" #'evil-snipe-repeat-reverse
  :n "`" #'magit-status ;; @TODO(renzix): Make this open in a new tab???
  :n "\\" #'projectile-find-file
- :n "g c" #'comment-line
  :n "g =" #'indent-buffer
- :nimv "M-h" #'evil-window-left
- :nimv "M-j" #'evil-window-down
- :nimv "M-k" #'evil-window-up
- :nimv "M-l" #'evil-window-right
- :nimv "M-s" #'evil-window-split
- :nimv "M-v" #'evil-window-vsplit
- :nimv "M-d" #'evil-delete-buffer
- :nimv "M-c" #'evil-window-delete
- :nimv "M-t" #'+workspace:new
- :nimv "M-T" #'+workspace:delete
- :nimv "M-]" #'+workspace:switch-next
- :nimv "M-[" #'+workspace:switch-previous
+ :nvimor "M-h" #'evil-window-left
+ :nvimor "M-j" #'evil-window-down
+ :nvimor "M-k" #'evil-window-up
+ :nvimor "M-l" #'evil-window-right
+ :nvimor "M-s" #'evil-window-split
+ :nvimor "M-v" #'evil-window-vsplit
+ :nvimor "M-d" #'evil-delete-buffer
+ :nvimor "M-c" #'evil-window-delete
+ :nvimor "M-t" #'+workspace:new
+ :nvimor "M-T" #'+workspace:delete
+ :nvimor "M-]" #'+workspace:switch-next
+ :nvimor "M-[" #'+workspace:switch-previous
  :n "SPC p x v" #'+vterm/open-popup-in-project) ;; idk why this isnt here already
 
 ;; Ex commands
-(evil-ex-define-cmd "config" 'doom/open-private-config)
-(evil-ex-define-cmd "buffer" '+ivy/switch-buffer)
+(evil-ex-define-cmd "conf[ig]" 'doom/open-private-config)
+(evil-ex-define-cmd "bl" '+ivy/switch-buffer)
+(evil-ex-define-cmd "buffer-list" '+ivy/switch-buffer)
 (evil-ex-define-cmd "rename" 'rename-current-buffer-and-file)
-(evil-ex-define-cmd "reload" 'doom/reload)
-(evil-ex-define-cmd "refresh" 'doom//refresh)
-(evil-ex-define-cmd "eshell" '+eshell/open-popup)
-(evil-ex-define-cmd "terminal" '+vterm/open-popup)
+(evil-ex-define-cmd "rn" 'rename-current-buffer-and-file)
+(evil-ex-define-cmd "rel[oad]" 'doom/reload)
+(evil-ex-define-cmd "ref[resh]" 'doom//refresh)
+(evil-ex-define-cmd "es[hell]" '+eshell/open-popup)
+(evil-ex-define-cmd "vt[erminal]" '+vterm/open-popup)
 
 ;; Package configuration
 (after! 'elcord
