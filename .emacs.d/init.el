c5a6d40119e18d17e0fba2017a54d135ca62d8b6
Renzix <eragon9981@gmail.com>
2019-08-29 00:02:36 -0400

    Edited it so it doesnt prompt on startup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,6 @@
 (if (<= emacs-major-version 26)
     (package-initialize)
   (package-refresh-contents))
+(setq enable-local-eval nil
+      enable-local-variables nil)
 (org-babel-load-file "~/Dotfiles/.emacs.d/config.org")
