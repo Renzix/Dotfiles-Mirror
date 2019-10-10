adefd96232f65ba523e2b1f28050a27ae26a991b
Renzix <eragon9981@gmail.com>
2019-10-10 13:44:20 -0400

    bored with powershell

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -48,7 +48,7 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "st -e pwsh"
+terminal = "st"
 editor = "emacs"
 browser = "firefox-nightly"
 
