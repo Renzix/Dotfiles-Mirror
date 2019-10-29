f4d005cd38f3a4645b14ca4f56282ed0b05e07a6
Renzix <eragon9981@gmail.com>
2019-10-28 22:52:26 -0400

    keyboard stuff

--- .zshrc
+++ .zshrc
@@ -15,8 +15,8 @@ compinit
 # End of lines added by compinstall
 # @TODO(Renzix): Make this change based on distro or highlight install
 # Syntax highlighting
-# source /usr/share/zsh/site-contrib/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # Gentoo
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # Arch
+source /usr/share/zsh/site-contrib/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # Gentoo
+# source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # Arch
 
 autoload -U colors && colors
 setopt PROMPT_SUBST
--- Bin/autostart/Keybinds.sh
+++ Bin/autostart/Keybinds.sh
@@ -1,19 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-# Emulates a buncha keybindings that i may or maynot use in the future????
+set -e
 
-# Bash XD all it does is get location of source file if a symlink
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+DISPLAY=":0.0"
+HOME=/home/genzix/
+export DISPLAY HOME
 
-TIMEOUT=1000
-xmodmap ${DIR}/.Xmodmap
-xmodmap -e "keycode any = Escape" 
-xmodmap -e "keycode any = Caps_Lock" 
-xmodmap -e "keycode any = Tab" 
+main() {
+    setxkbmap -option ctrl:nocaps
+    xcape -e 'Control_L=Escape'
+    xcape -e 'Shift_L=BackSpace'
+}
 
+main "$@"
