b20e3d8d7136823eba34012a69a66ce5f49e7bd7
Renzix <DanielDeBruno@renzix.com>
2021-04-19 22:02:27 -0400

    Added a new bar 4 time

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,6 +3,7 @@
 killall -q polybar
 echo "---" | tee -a /tmp/polybar1.log
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bar3 2>&1 | tee -a /tmp/polybar3.log & disown
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
