bcf158de40fa0eb7ec98bf905fae0184533009a1
Renzix <DanielDeBruno@renzix.com>
2021-04-22 21:55:14 -0400

    updated polybar and dunstrc

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,13 +2,14 @@
 
 killall -q polybar
 echo "---" | tee -a /tmp/polybar1.log
+echo "---" | tee -a /tmp/polybar2-1.log
+echo "---" | tee -a /tmp/polybar2-2.log
+echo "---" | tee -a /tmp/polybar2-3.log
+echo "---" | tee -a /tmp/polybar3.log
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar bar3 2>&1 | tee -a /tmp/polybar3.log & disown
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
-  done
-else
-  polybar bar2 2>&1 | tee -a /tmp/polybar1.log & disown
-fi
+# probably could do this better but the resolutions arent the same
+MONITOR=DVI-D-0 WIDTH=12% OFFSET=44% polybar bar2 2>&1 | tee -a /tmp/polybar2-1.log & disown
+MONITOR=DP-2 WIDTH=12.5% OFFSET=43.75% polybar bar2 2>&1 | tee -a /tmp/polybar2-2.log & disown
+MONITOR=DP-4 WIDTH=12% OFFSET=44% polybar bar2 2>&1 | tee -a /tmp/polybar2-3.log & disown
 echo "Bar launched"
---
+++ .config/polybar/scripts/search.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+rofi -show-icons -show $1
